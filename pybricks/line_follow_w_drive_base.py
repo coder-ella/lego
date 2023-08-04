7d33769289c6a90a7bcb1bf7bbd9d49a02a27f1f
coder-ella <66184276+coder-ella@users.noreply.github.com>
2023-08-04 16:27:22 -0600

    Commented line_follow_w_drive_base.py

--- pybricks/line_follow_w_drive_base.py
+++ pybricks/line_follow_w_drive_base.py
@@ -7,6 +7,7 @@ from pybricks.tools import wait, StopWatch
 #Clear terminal
 print("\x1b[H\x1b[2J", end="")
 
+#initialize hub, motors, sensors, drivebase
 hub = InventorHub()
 motor_left = Motor(Port.A, Direction.COUNTERCLOCKWISE)
 motor_right = Motor(Port.D,Direction.CLOCKWISE)
@@ -17,20 +18,21 @@ sensor_2 = ColorSensor(Port.E)
 bob = DriveBase(left_motor = motor_left, right_motor=motor_right,
 wheel_diameter = 55.6, axle_track = 95)
 
+#Minimum reflection to turn, 55 is between white and black
 THRESHOLD = 55
+#Line follow speed
 SPEED = 140
-KP = 1
 #Constant of proportionality for moving
-exit_var = 0
+KP = 1
 #How many times right color sensor has seen black in a row
-
+exit_var = 0
 #55, 0.25 - 43, 0.25 work for THRESHOLD, SPEED, and KP 
-
+#Set settings
 bob.settings(straight_speed=100,
 straight_acceleration=100)
 
+#Go to the line from home base
 bob.straight(50)
-#Go to the line from home
 
 while True:
     wait(1)
@@ -40,20 +42,26 @@ while True:
     print(f"rl: {refl_left} rr: {refl_right} s: {steering} b: {exit_var}")
     bob.drive(SPEED,-steering)
     
+    #<= 15 is black
     if refl_right <= 15:
         exit_var += 1
     else:
         exit_var = 0
+    #Takes sensor 10 times of seeing black for back line to pass.
     if exit_var >= 10:
         break
 #Waits for right sensor to see a black line
 motor_left.hold()
 motor_right.hold()
 
+#Change turn speed
 bob.settings(turn_rate=100)
+#Go past line
 bob.straight(50)
+#Turn toward other line
 bob.turn(100)
 
+#Follow next line
 while True:
     wait(1)
     refl_left = sensor.reflection()
