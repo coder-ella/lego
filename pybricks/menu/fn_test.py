a0b989706e635dc6c8fbcc38fb6c14224ee7281b
Raj <raj.m.rao@gmail.com>
2023-08-04 21:39:45 -0600

    Update fn_test.py

--- pybricks/menu/fn_test.py
+++ pybricks/menu/fn_test.py
@@ -1,3 +1,6 @@
+# this file represents a template for writing code that can then be imported
+# and called from the main_menu.
+
 from pybricks.hubs import InventorHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
@@ -17,6 +20,8 @@ def turn_45_l(bob):
     bob.turn(-45, then=Stop.COAST)
 
 if __name__ == '__main__':
+    # this section of the code allows for testing of this file
+    # without needing to use the main-menu
     print("testing")
     hub = InventorHub()
     motor_left = Motor(Port.A, Direction.COUNTERCLOCKWISE)
@@ -28,7 +33,6 @@ if __name__ == '__main__':
     motor_attach_right = Motor(Port.B, Direction.CLOCKWISE,gears=[20,12])
     sensor = ColorSensor(Port.F)
     sensor_2 = ColorSensor(Port.E)
-
 
     move_forward(bob)
     turn_45_l(bob)
