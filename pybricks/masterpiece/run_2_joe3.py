e8119c61416a0d8203eb51fa59cdadc29d39c6fd
coder-ella <66184276+coder-ella@users.noreply.github.com>
2023-10-19 12:42:18 -0600

    Update run_2_joe3.py

--- pybricks/masterpiece/run_2_joe3.py
+++ pybricks/masterpiece/run_2_joe3.py
@@ -1,9 +1,14 @@
 from pybricks.hubs import InventorHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
-from pybricks.robotics import DriveBase
+from pybricks.robotics import DriveBase, GyroDriveBase
 from pybricks.tools import wait, StopWatch
 
+"""
+Robot: Not Bob
+Home: Left side of board
+Attachments: Front fork + original mantis arm
+"""
 
 def Everett_Grace_Function(hub, bob, moyrorL):
     # change the name of this function to what you are doing
@@ -84,6 +89,7 @@ if __name__ == '__main__':
     motor_left = Motor(Port.C, Direction.COUNTERCLOCKWISE)
     motor_right = Motor(Port.D,Direction.CLOCKWISE)
     moyrorL = Motor(Port.A)
+    #try GyroDriveBase
     bob = DriveBase(left_motor = motor_left, right_motor=motor_right, wheel_diameter = 55.6, axle_track = 83.79999999699997)
     bob.settings(turn_acceleration=450)
     Everett_Grace_Function(hub, bob, moyrorL)
