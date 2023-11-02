80d221408a669a27545ce52bffe5dfa467380615
Raj <raj.m.rao@gmail.com>
2023-11-02 16:26:55 -0600

    Update A_clean_wheels.py

--- pybricks/masterpiece/A_clean_wheels.py
+++ pybricks/masterpiece/A_clean_wheels.py
@@ -9,6 +9,15 @@ from pybricks.tools import wait, StopWatch
 def clean(hub, bob):
     wait(200)
     bob.straight(700)
+    wait(200)
+    bob.straight(-700)
+    
+    hub.speaker.play_notes([
+    "G3/4", "G3/4", "G3/4", "C3/4.",  
+    "G3/4","F3/4","E3/4","D3/4", "C4/4.",
+    "G3/4","F3/4","E3/4","D3/4", "C4/4.",
+    "G3/4","F3/4","E3/4","F3/4","D3/4.", 
+    ])
 
 
 # this code allows you to run this code directly without using
@@ -16,6 +25,8 @@ def clean(hub, bob):
 if __name__ == '__main__':
     print("testing")
     hub = InventorHub()
+    motor_left = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+    motor_right = Motor(Port.D,Direction.CLOCKWISE)
     bob = DriveBase(left_motor = motor_left, right_motor=motor_right,
     wheel_diameter = 55.6, axle_track = 83.79999999699997)
     clean(hub, bob)
