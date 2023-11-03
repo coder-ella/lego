7346ac2a78069c5c7e7fbfdb5f425e3306c6c244
Raj <raj.m.rao@gmail.com>
2023-11-02 19:52:26 -0600

    Update A_clean_wheels.py

--- pybricks/masterpiece/A_clean_wheels.py
+++ pybricks/masterpiece/A_clean_wheels.py
@@ -5,29 +5,50 @@ from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
 # https://docs.pybricks.com/en/latest/robotics.html
+
 '''
 # default settings
 bob.settings(straight_speed=194, straight_acceleration=727
     ,turn_rate=221, turn_acceleration=995)
+
+#max values allowed
+bob.settings(straight_speed=970, straight_acceleration=9704
+    ,turn_rate=1327, turn_acceleration=13271) 
+# high speed
+bob.settings(straight_speed=485, straight_acceleration=9704
+    ,turn_rate=663, turn_acceleration=13271) 
 '''
+
 def clean(hub, bob):
-    wait(200)
-    bob.straight(700)
-    wait(200)
-    bob.straight(-700)
+    # Clear terminal
+    print("\x1b[H\x1b[2J", end="")
+    default_settings = bob.settings()
+    print(default_settings)
     
+    bob.settings(straight_speed=485, straight_acceleration=9704,turn_rate=663, turn_acceleration=13271) 
+    wait(200)
+    bob.straight(2000)
+
+    bob.settings(straight_speed=485, straight_acceleration=9704,turn_rate=663, turn_acceleration=13271) 
+    wait(200)
+    bob.straight(-2000)
+
+    bob.settings(default_settings[0],default_settings[1],default_settings[2],default_settings[3])
+    bob.straight(1000)
+        
     hub.speaker.play_notes([
     "G3/4", "G3/4", "G3/4", "C3/4.",  
     "G3/4","F3/4","E3/4","D3/4", "C4/4.",
     "G3/4","F3/4","E3/4","D3/4", "C4/4.",
     "G3/4","F3/4","E3/4","F3/4","D3/4.", 
     ])
+    
+    print("done")
 
 
 # this code allows you to run this code directly without using
 # the menu system
 if __name__ == '__main__':
-    print("testing")
     hub = InventorHub()
     motor_left = Motor(Port.C, Direction.COUNTERCLOCKWISE)
     motor_right = Motor(Port.D,Direction.CLOCKWISE)
