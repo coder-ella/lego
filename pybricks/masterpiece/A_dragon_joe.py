a49f4da7d1de34de31df959a93eb9914bc480cec
coder-ella <66184276+coder-ella@users.noreply.github.com>
2023-11-05 17:41:30 -0700

    Create A_dragon_joe.py

---
+++ pybricks/masterpiece/A_dragon_joe.py
@@ -0,0 +1,62 @@
+from pybricks.hubs import InventorHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+"""
+Robot: Joe
+Home: Left side of board
+Attachments: Front fork + original mantis arm
+"""
+
+def dragon_run(hub, bob, moyrorL):
+    # change the name of this function to what you are doing
+    # add required parameters like hub, bob
+    # eg of a good name: do_mission_run_1(hub, bob)
+    # update the name of your function, 
+    # at the end of the code.
+
+    #Clear terminal
+    print("\x1b[H\x1b[2J", end="")
+
+    bob.use_gyro(True)
+    
+    print("Lunching")
+    #example
+    #bob.straight(200, then=Stop.COAST)
+    print(f"bob settings {bob.settings()}")
+    # bob settings (st.speed 207, st.accl 720, turn.accl 238, turn rate 450)
+    bob.settings(straight_speed=300)
+    hub.light.on(Color.GREEN)
+
+    moyrorL.run_angle(600,20)
+
+    bob.straight(300)
+
+    bob.turn(-25)
+
+    bob.straight(60)
+    
+    bob.turn(37)
+    
+    bob.straight(-600)
+    
+
+
+    #print("\x1b[H\x1b[2J", end="")
+    print("Lunch Complete")
+
+    bob.use_gyro(False)
+
+if __name__ == '__main__':
+    print("Preparing to Lunch")
+    hub = InventorHub()
+    motor_left = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+    motor_right = Motor(Port.D,Direction.CLOCKWISE)
+    moyrorL = Motor(Port.A)
+    #try GyroDriveBase
+    bob = DriveBase(left_motor = motor_left, right_motor=motor_right, 
+    wheel_diameter = 55.6, axle_track = 83.79999999699997)
+    bob.settings(turn_acceleration=450)
+    dragon_run(hub, bob, moyrorL)
