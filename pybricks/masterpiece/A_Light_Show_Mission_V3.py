0d1621e9e41215200516055367f409dc400cf831
coder-ella <66184276+coder-ella@users.noreply.github.com>
2023-11-06 20:42:21 -0700

    Add files via upload

---
+++ pybricks/masterpiece/A_Light_Show_Mission_V3.py
@@ -0,0 +1,122 @@
+from pybricks.hubs import InventorHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop, Icon       
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+# https://docs.pybricks.com/en/latest/robotics.html
+
+#CHANGES TO MAKE
+# - Move less toward augmented reality
+# - Perfect turn to get to setup for light show
+# - 
+
+def light_show_run(hub, bob, moyrorR, moyrorL):
+    """
+    function for run 1
+
+    
+    Arguments:
+        hub: the hub
+        bob: the drive base
+        moyrorR: The right attachment motor
+    """
+    ratio = 1.8
+    bob.use_gyro(True)
+    #Clear terminal
+    print("\x1b[H\x1b[2J", end="")
+    
+    print("...")
+    bob.settings(straight_acceleration= 300, turn_acceleration= 200)
+    hub.light.on(Color.GREEN)
+    print("Wallsquare")
+    bob.straight(-50)
+    print("Go toward printer")
+    bob.straight(330)
+    print("Turn toward printer")
+    bob.turn(-45)
+    print("Ram printer")
+    bob.straight(200)
+    print("Back away from printer")
+    bob.straight(-100)
+    print("Turns to go forward")
+    bob.turn(40)
+    print("Forward toward hologram mission")
+    bob.straight(325)
+    print("Turn toward hologram mission")
+    bob.turn(-130)
+    print("Ram hologram mission")
+    bob.straight(-350)
+    print("Back away from hologram mission")
+    bob.straight(50)
+    print("Turn to go towards light show mission")
+    bob.turn(35)
+    print("Go towards light show mission")
+    bob.straight(525)
+    print("Augmented Fakeality Mission")
+    
+    bob.turn(105)
+    bob.straight(60)
+    bob.settings(straight_speed=900)
+    
+    moyrorR.run_angle(1001, 650*ratio)
+    
+    bob.settings(straight_speed=600)
+    bob.straight(-60)
+    bob.settings(straight_speed=300)
+    
+    print("go to Light Show Mission")
+
+    bob.turn(90)
+    moyrorR.run_angle(-1000, 700*ratio)
+    bob.straight(-180)
+    bob.turn(90)
+    bob.straight(80)
+    print("Do light show mission")
+    moyrorL.run_angle(10000, 3400)
+    bob.straight(-500)
+    #bob.settings(straight_speed=60)
+    #bob.straight(100)
+    hub.speaker.beep()
+    print("Turn towards camera mission") 
+        
+    print("Grab Camera")
+    
+    bob.straight(100)
+    bob.turn(21)
+    bob.straight(705)
+    print("Turn")
+    bob.turn(-20)
+    bob.straight(40)
+    moyrorR.run_angle(9900, 675*ratio)
+    bob.turn(70)
+    moyrorR.run_angle(2982, -700*ratio)
+    bob.turn(20)
+    bob.straight(500)
+    bob.turn(-60)
+    bob.straight(1000)
+
+
+    print("done")
+    hub.speaker.play_notes(["C4/4", "C4/4", "G4/4", "G4/4"])
+    hub.display.icon(Icon.HAPPY)
+    wait(200)
+    bob.use_gyro(False)
+    
+
+# this code allows you to run this code directly without using
+# the menu system
+if __name__ == '__main__':
+    print("testing")
+    hub = InventorHub()
+    motor_left = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+    motor_right = Motor(Port.D,Direction.CLOCKWISE)
+    moyrorR = Motor(Port.B)
+    moyrorL = Motor(Port.A)
+    bob = DriveBase(left_motor = motor_left, right_motor=motor_right,
+    wheel_diameter = 55.6, axle_track = 83.79999999699997)
+
+    # call your function.
+    # remember to rename the below name to match
+    # your function name on line 11
+    light_show_run(hub, bob, moyrorR)
---
+++ pybricks/masterpiece/A_Long_Poker_Thing.py
@@ -0,0 +1,59 @@
+from pybricks.hubs import InventorHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+"""
+Robot: Not Bob
+Home: Left side of board
+Attachments: Front fork + original mantis arm
+"""
+
+def longy(hub, bob, moyrorL):
+    # change the name of this function to what you are doing
+    # add required parameters like hub, bob
+    # eg of a good name: do_mission_run_1(hub, bob)
+    # update the name of your function, 
+    # at the end of the code.
+
+    #Clear terminal
+    print("\x1b[H\x1b[2J", end="")
+
+    print(f"Ready: {hub.imu.ready()}")
+
+    bob.use_gyro(True)
+    
+    print("Lunching")
+    #example
+    #bob.straight(200, then=Stop.COAST)
+    print(f"bob settings {bob.settings()}")
+    # bob settings (st.speed 207, st.accl 720, turn.accl 238, turn rate 450)
+    bob.settings(straight_acceleration=250, turn_acceleration=100,turn_rate=200)
+    hub.light.on(Color.GREEN)
+    
+
+    bob.settings(75)
+    bob.straight(380)
+    bob.settings(900)
+    bob.straight(-200)
+    bob.turn(25)
+    bob.straight(75)
+    bob.turn(15)
+    bob.straight(100)
+    bob.turn(30)
+    bob.straight(125)
+    bob.turn(120)
+    bob.straight(200)
+
+    print("me done now")
+if __name__ == '__main__':
+    print("Preparing to Lunch")
+    hub = InventorHub()
+    motor_left = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+    motor_right = Motor(Port.D,Direction.CLOCKWISE)
+    moyrorL = Motor(Port.A)
+    #try GyroDriveBase
+    bob = DriveBase(left_motor = motor_left, right_motor=motor_right, 
+    wheel_diameter = 55.6, axle_track = 83.79999999699997)
+    bob.settings(turn_acceleration=450)
+    longy(hub, bob, moyrorL)
